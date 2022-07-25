8feeecb1246dc633ec7017356c8b7a51d179eb48
teradaian <teradaian@gmail.com>
2022-07-25 14:28:31 -0700

    refactor PostList into component parts

--- src/pages/Post/PostList/PostList.jsx
+++ src/pages/Post/PostList/PostList.jsx
@@ -1,12 +1,9 @@
 import { useState, useEffect } from 'react'
 import Post from '../Post'
 import Box from '@mui/material/Box'
-import Card from '@mui/material/Card'
-import CardContent from '@mui/material/CardContent'
-import Skeleton from '@mui/material/Skeleton'
-import Button from '@mui/material/Button'
-import Pagination from '@mui/material/Pagination'
+import PaginatedList from './components/PaginatedList'
 import PostTopMenu from './components/PostTopMenu'
+
 import { seedData } from '../../../assets/seedData/seedData'
 
 const PostList = ({ user }) => {
@@ -25,43 +22,16 @@ const PostList = ({ user }) => {
     <Post key={uuid} post={post} />
   ))
 
-  const skeleton = [...'placeholdr'].map((skeleton, uuid) => (
-    <Card 
-      sx={{ maxWidth: 275, height: 325 }} 
-      key={uuid} 
-      style={{ padding: '1rem' }}
-    >
-      <CardContent style={{ display: 'flex', justifyContent: 'space-between', padding: '0 0 16px 0'}}>
-        <Skeleton animation="wave" height={30} width="60%" />
-        <Skeleton animation="wave" height={30} width="30%" />
-      </CardContent>
-      <Skeleton variant="rectangular" animation="wave" width={245} height={180} />
-      <Skeleton animation="wave" width={245} height={60} style={{ marginTop: '12px' }} />
-    </Card>
-  ))
-
   return ( 
-    <Box style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', margin: '3rem'}}>
-      {/* This should get split into two, or maybe three components*/}
-      {/* First Component */}
+    <Box style={{ 
+      display: 'flex', 
+      flexDirection: 'column', 
+      justifyContent: 'center', 
+      alignItems: 'center', 
+      margin: '3rem'
+    }}>
       <PostTopMenu postTitles={postTitles} />
-      {/* Second Component */}
-      <Button onClick={() => setLoading(!loading)}> Toggle Loading </Button>
-      <Box style={{ 
-        display: 'flex', 
-        gap: '1rem', 
-        flexWrap: 'wrap', 
-        marginBottom: '1.5rem' 
-      }}>
-        { loading 
-          ? skeleton
-          : seedPosts
-        }
-      </Box>
-      <Pagination 
-        count={5}
-        color="primary"
-      />
+      <PaginatedList loading={loading} setLoading={setLoading} seedPosts={seedPosts} />
     </Box>
    );
 }
