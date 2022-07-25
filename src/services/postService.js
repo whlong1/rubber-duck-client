fb680cfecf3957ccbcff5cc1107174763e2887f1
whlong1 <whunterlong@gmail.com>
2022-07-25 11:54:10 -0400

    build out initial postService functions

--- src/services/postService.js
+++ src/services/postService.js
@@ -2,5 +2,76 @@ import * as tokenService from '../services/tokenService'
 
 const BASE_URL = `${process.env.REACT_APP_BACK_END_SERVER_URL}/api/posts`
 
+export const create = async (post) => {
+  // this service function requires a topicId in submitted formData
+  try {
+    const res = await fetch(BASE_URL, {
+      method: "POST",
+      headers: {
+        'content-type': 'application/json',
+        'Authorization': `Bearer ${tokenService.getToken()}`
+      },
+      body: JSON.stringify(post),
+    })
+    return await res.json()
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
+}
 
-export { }
+export const index = async () => {
+  // controller built to accept queries:
+  // search, page, sort
+  try {
+    const res = await fetch(BASE_URL, {
+      headers: {
+        'Authorization': `Bearer ${tokenService.getToken()}`
+      },
+    })
+    return await res.json()
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
+}
+
+export const show = async (id) => {
+  try {
+    const res = await fetch(`${BASE_URL}/${id}`, {
+      headers: {
+        'Authorization': `Bearer ${tokenService.getToken()}`
+      },
+    })
+    return await res.json()
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
+}
+
+export const incrementViews = async (id) => {
+  try {
+    const res = await fetch(`${BASE_URL}/${id}/views`, {
+      method: "PATCH",
+      headers: {
+        'Authorization': `Bearer ${tokenService.getToken()}`
+      },
+    })
+    return await res.json()
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
+}
+
+
+
+export {
+  create,
+  index,
+  show,
+  incrementViews
+
+
+ }
--- src/services/topicService.js
+++ src/services/topicService.js
@@ -35,7 +35,7 @@ export const index = async () => {
 
 export const show = async (id) => {
   try {
-    const res = await fetch(`${BASE_URL}${id}`, {
+    const res = await fetch(`${BASE_URL}/${id}`, {
       headers: {
         'Authorization': `Bearer ${tokenService.getToken()}`
       },
