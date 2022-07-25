04a89650925031930bb5188cc3738a8d26d7a443
teradaian <teradaian@gmail.com>
2022-07-25 09:46:04 -0700

    port imports from landing to post

---
+++ src/pages/Post/Post.jsx
@@ -0,0 +1,25 @@
+import Button from '@mui/material/Button'
+import Card from '@mui/material/Card'
+import Box from '@mui/material/Box'
+import CardContent from '@mui/material/CardContent'
+import Typography from '@mui/material/Typography'
+import CardActions from '@mui/material/CardActions'
+import CheckBox from '@mui/material/Checkbox'
+import Rating from '@mui/material/Rating'
+
+
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorderOutlined'
+import BookmarkIcon from '@mui/icons-material/Bookmark'
+import DiamondRoundedIcon from '@mui/icons-material/DiamondRounded'
+import DiamondOutlinedIcon from '@mui/icons-material/DiamondOutlined'
+
+import { ratingOptions } from '../../styles/theme'
+import { styled } from '@mui/material/styles'
+import styles from './Landing.module.css'
+
+
+const Post = () => {
+  return (  );
+}
+ 
+export default Post;
