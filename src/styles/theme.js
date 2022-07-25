6471c084e2bd29a2ae0d67534a38631da57c5d2e
teradaian <teradaian@gmail.com>
2022-07-25 08:34:51 -0700

    install mui deps

---
+++ src/styles/theme.js
@@ -0,0 +1,18 @@
+export const themeOptions = {
+  palette: {
+    type: 'dark',
+    primary: {
+      main: '#ffc82f',
+    },
+    secondary: {
+      main: '#ffffff',
+    },
+    background: {
+      default: '#232327',
+      paper: 'rgba(66,66,66,0.46)',
+    },
+    success: {
+      main: '#4caf50',
+    }
+  }
+}
