9e03a5c8350e35842bca6ea2b2943f981cb3a62a
whlong1 <whunterlong@gmail.com>
2022-07-26 16:58:27 -0400

    add profanity check

--- src/pages/NewIteration/utils/utils.js
+++ src/pages/NewIteration/utils/utils.js
@@ -40,4 +40,17 @@ export const writingTips = {
     },
     explanation: 'Try to avoid making assumptions about what your reader knows.'
   },
+  profanity: {
+    fn: function (text) {
+      const suggestions = []
+      const positives = ['Fuck', 'Shit', 'Ass', 'Bitch']
+      const regex = new RegExp('\\b(' + positives.join('|') + ')\\b', 'gi')
+      const match = regex.exec(text)
+      if (match) {
+        suggestions.push({ index: match.index, offset: match[0].length, })
+      }
+      return suggestions
+    },
+    explanation: 'Please avoid using profanity.'
+  },
 }
