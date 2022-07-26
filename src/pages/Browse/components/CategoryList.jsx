3815b9f704b79fbe015f09b0aabd5829ee97d577
teradaian <teradaian@gmail.com>
2022-07-26 11:33:00 -0700

    init style cards

--- src/pages/Browse/components/CategoryList.jsx
+++ src/pages/Browse/components/CategoryList.jsx
@@ -1,10 +1,26 @@
+import Stack from '@mui/material/Stack'
+import Card from '@mui/material/Card'
+
 const CategoryList = (props) => {
   return (
     <>
       Categories:
+      <Stack
+        direction="row"
+        spacing={1}
+        sx={{ display: 'flex', justifyContent: { md: 'space-evenly', xs: 'space-around' } }}
+      >
       {props.categories.map((category, idx) => (
-        <button key={idx} onClick={() => props.setSelected(category)}>{category}</button>
+          <Card
+            key={idx} 
+            variant="contained"
+            sx={{ height: { md: 250, xs: 150 }, width: { md: 200, xs: 100}, cursor: 'pointer' }}
+            onClick={() => props.setSelected(category)}
+          >
+              {category}
+          </Card>
       ))}
+      </Stack>
     </>
   )
 }
