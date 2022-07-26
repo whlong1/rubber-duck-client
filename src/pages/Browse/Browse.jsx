6e863cca07db28f48634230d02b41f6ac124fd77
whlong1 <whunterlong@gmail.com>
2022-07-26 11:18:38 -0400

    stub up usestate and useeffect

--- src/pages/Browse/Browse.jsx
+++ src/pages/Browse/Browse.jsx
@@ -1,4 +1,17 @@
+import { useState, useEffect } from 'react'
+import * as topicService from '../../services/topicService'
+
 const Browse = () => {
+  const [topics, setTopics] = useState([])
+
+  useEffect(() => {
+    const fetchTopics = async () => {
+      const data = await topicService.index(search)
+      console.log(data)
+    }
+    return () => fetchTopics()
+  }, [])
+
   return (
     <>
     </>
