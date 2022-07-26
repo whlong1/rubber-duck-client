2dc8a7eac29d59d84efb7760f692437b080d6936
whlong1 <whunterlong@gmail.com>
2022-07-26 14:38:16 -0400

    build out new post component

--- src/pages/NewPost/Newpost.jsx
+++ src/pages/NewPost/Newpost.jsx
@@ -1,12 +1,17 @@
 import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 
 // Components
 import PostForm from './components/PostForm/PostForm'
 import Analysis from './components/Analysis/Analysis'
 
+import * as postService from '../../services/postService'
+
+
 function NewPost() {
+  const { topicId } = useParams()
   const [text, setText] = useState('')
-  const [keywords, setKeywords] = useState(null)
+  // const [keywords, setKeywords] = useState(null)
 
   const handleSubmit = async (e) => {
     e.preventDefault()
@@ -15,22 +20,19 @@ function NewPost() {
 
   useEffect(() => {
     const fetchKeyWords = () => {
-      const arr = [
-        { word: 'for loop', score: 5 },
-        { word: 'repeatedly', score: -1 },
-        { word: 'washing machine', score: -10 },
-      ]
-      setKeywords(arr)
+      const data = await postService.findKeywords(topicId)
+      console.log('keyword data::::', data)
+      // setKeywords(arr)
     }
     fetchKeyWords()
-  }, [])
+  }, [topicId])
 
   return (
     <div>
       <h1>Category</h1>
       <h3>Topic</h3>
-      <PostForm text={text} setText={setText} handleSubmit={handleSubmit} />
-      <Analysis text={text} keywords={keywords} />
+      {/* <PostForm text={text} setText={setText} handleSubmit={handleSubmit} />
+      <Analysis text={text} keywords={keywords} /> */}
     </div>
   )
 }
