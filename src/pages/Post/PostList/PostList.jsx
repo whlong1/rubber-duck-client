4a482edd87bc2921150890104b8761f8abeb24d0
whlong1 <whunterlong@gmail.com>
2022-07-26 11:53:07 -0400

    add useParams instance to PostList

--- src/pages/Post/PostList/PostList.jsx
+++ src/pages/Post/PostList/PostList.jsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+
 import Post from '../Post'
 import Box from '@mui/material/Box'
 import Divider from '@mui/material/Divider'
@@ -9,7 +11,12 @@ import TungstenIcon from '@mui/icons-material/Tungsten';
 
 import { seedData } from '../../../assets/seedData/seedData'
 
+
 const PostList = ({ user }) => {
+  const { topicId } = useParams()
+  console.log(topicId)
+
+
   const [loading, setLoading] = useState(true)
   const [postTitles, setPostTitles] = useState([])
 
@@ -25,19 +32,19 @@ const PostList = ({ user }) => {
     <Post key={uuid} post={post} />
   ))
 
-  return ( 
-    <Box style={{ 
-      display: 'flex', 
-      flexDirection: 'column', 
-      justifyContent: 'center', 
-      alignItems: 'center', 
+  return (
+    <Box style={{
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
       margin: '3rem'
     }}>
       <PostTopMenu postTitles={postTitles} />
       <Divider textAlign="left" sx={{ color: 'primary', width: '100%', marginBottom: '1rem' }}><TungstenIcon color="primary" /></Divider>
       <PaginatedList loading={loading} setLoading={setLoading} seedPosts={seedPosts} />
     </Box>
-   );
+  );
 }
- 
+
 export default PostList;
