0533355889595d5e90930b2926a8e0dbb4b716f7
whlong1 <whunterlong@gmail.com>
2022-07-27 11:25:40 -0400

    add react lottie

---
+++ src/pages/Landing/Arrow.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+import Lottie from "lottie-react"
+import arrow from '../../assets/arrow-anim.json'
+
+const Arrow = () => <Lottie animationData={arrow} loop={true} />
+
+export default Arrow
--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,19 +1,21 @@
-import styles from './Landing.module.css'
-import logo from '../../assets/rubberDuckLogo.png'
+import'./Landing.css'
+// import logo from '../../assets/rubberDuckLogo.png'
+import logo from '../../assets/logo-icon.svg'
 import feynman from '../../assets/feynman.png'
 import rectangle from '../../assets/yellowRectangle.png'
-import DuckAnimation from '../../components/DuckAnimation/DuckAnimation'
+
+import Arrow from './Arrow'
 
 const Landing = ({ user }) => {
   return (
-    <div className={styles.main}>
-      <div className={styles.bodyContainer}>
+    <div className='container'>
+      <div className='splash-container'>
         <img src={logo} alt='logo' />
-        <h1 className={styles.logoH1}>Rubber Duck</h1>
-        <DuckAnimation/>
+        <h1 className='logoTitle'>Rubber Duck</h1>
       </div>
+      <Arrow/>
 
-      <div className={styles.logoH1}>
+      {/* <div className={styles.logoH1}>
         <img src={rectangle} alt="rectangle" style={{ height: 30, width: 70 }} />
         <h1 className={styles.WTFduck}>What the $%!# is Rubber Duck? </h1>
         <div className={styles.flex}>
@@ -46,7 +48,7 @@ const Landing = ({ user }) => {
           <p>The Feynman Learning Technique is a simple way of approaching anything new you want to learn. Why use it? Because learning doesn't happen from skimming through a book or remembering enough to pass a test. Information is learned when you can explain it and use it in a wide variety of situations. The Feynman Technique gets more mileage from the ideas you encounter instead of rendering anything new into isolated, useless factoids.</p>
         </div>
       </div>
-      <div className={styles.whitespace}></div>
+      <div className={styles.whitespace}></div> */}
     </div>
   )
 }
