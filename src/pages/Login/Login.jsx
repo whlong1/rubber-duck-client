8bfb615930b058f5b00934c8d9b7754fae188760
teradaian <teradaian@gmail.com>
2022-07-27 08:05:51 -0700

    change out login text

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -11,7 +11,7 @@ const LoginPage = props => {
 
   return (
     <main className={styles.container}>
-      <h1>Log In</h1>
+      <h1>Welcome back</h1>
       <p>{message}</p>
       <LoginForm
         handleSignupOrLogin={props.handleSignupOrLogin}
