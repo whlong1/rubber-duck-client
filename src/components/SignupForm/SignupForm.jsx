346d74aac42b40f0e15b632c1b10ab552d1ac699
teradaian <teradaian@gmail.com>
2022-07-26 22:58:34 -0700

    signup form initial staging

--- src/components/SignupForm/SignupForm.jsx
+++ src/components/SignupForm/SignupForm.jsx
@@ -9,6 +9,7 @@ import Button from '@mui/material/Button'
 import FormControl from '@mui/material/FormControl'
 import InputLabel from '@mui/material/InputLabel'
 import OutlinedInput from '@mui/material/OutlinedInput'
+import Paper from '@mui/material/Paper'
 import InputAdornment from '@mui/material/InputAdornment'
 import IconButton from '@mui/material/IconButton'
 import Visibility from '@mui/icons-material/Visibility'
@@ -59,134 +60,139 @@ const SignupForm = props => {
   }
 
   return (
-    <form
-      autoComplete="off"
-      onSubmit={handleSubmit}
-      className={styles.container}
+    <Paper 
+      sx={{ width: '600px', height: '700px', paddingTop: '2rem', backgroundColor: '#000000' }} 
+      elevation={15}
     >
-      <Box className={styles.inputContainer}>
-        <TextField
-          fullWidth
-          type="text"
-          label='Name'
-          autoComplete="off"
-          id="name"
-          value={name}
-          name="name"
-          onChange={handleChange}
-        />
-      </Box>
-      <Box className={styles.inputContainer}>
-        <TextField
-          fullWidth
-          type="text"
-          label="Email"
-          autoComplete="off"
-          id="email"
-          value={email}
-          name="email"
-          onChange={handleChange}
-        />
-      </Box>
-      <Box className={styles.inputContainer}>
-      <FormControl sx={{ width: '400px'}} variant="outlined">
-          <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
-          <OutlinedInput
-            id="outlined-adornment-password"
-            name='password'
-            type={showPassword ? 'text' : 'password'}
-            value={password}
+      <form
+        autoComplete="off"
+        onSubmit={handleSubmit}
+        className={styles.container}
+      >
+        <Box className={styles.inputContainer}>
+          <TextField
+            fullWidth
+            type="text"
+            label='Name'
+            autoComplete="off"
+            id="name"
+            value={name}
+            name="name"
             onChange={handleChange}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={() => setShowPassword(!showPassword)}
-                  onMouseDown={handleMouseDownPassword}
-                  edge="end"
-                >
-                  {showPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            }
-            label="Password"
           />
-        </FormControl>
-      </Box>
-      <Box className={styles.inputContainer}>
-      <FormControl sx={{ width: '400px'}} variant="outlined">
-          <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
-          <OutlinedInput
-            id="outlined-adornment-conf-password"
-            name='passwordConf'
-            type={showConfPassword ? 'text' : 'password'}
-            value={passwordConf}
+        </Box>
+        <Box className={styles.inputContainer}>
+          <TextField
+            fullWidth
+            type="text"
+            label="Email"
+            autoComplete="off"
+            id="email"
+            value={email}
+            name="email"
             onChange={handleChange}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={() => setShowConfPassword(!showConfPassword)}
-                  onMouseDown={handleMouseDownPassword}
-                  edge="end"
-                >
-                  {showConfPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            }
-            label="Password"
           />
-        </FormControl>
-      </Box>
-      <Box className={styles.inputContainer}>
-        <TextField
-          fullWidth
-          label="Occupation"
-          type="text"
-          id="occupation"
-          name="occupation"
-          onChange={handleChange}
-        />
-      </Box>
-      <Box className={styles.inputContainer}>
-        <TextField
-          fullWidth
-          helperText="Date of Birth"
-          type="date"
-          id="dob"
-          name="dob"
-          onChange={handleChange}
-        />
-      </Box>
-      <Box className={styles.inputContainer}>
-        <TextField
-          fullWidth
-          label="Postal Zip Code"
-          type="number"
-          id="zip"
-          name="zip"
-          onChange={handleChange}
-        />
-      </Box>
-      <Box className={styles.inputContainer}>
-        <TextField
-          fullWidth
-          label="Education"
-          type="text"
-          id="education"
-          name="education"
-          onChange={handleChange}
-        />
-      </Box>
-      <Box className={styles.inputContainer}>
-        <Button disabled={isFormInvalid()} className={styles.button}>
-          Sign Up
-        </Button>
-        <Link to="/">
-          <Button color="warning">Cancel</Button>
-        </Link>
-      </Box>
-    </form>
+        </Box>
+        <Box className={styles.inputContainer}>
+        <FormControl sx={{ width: '400px'}} variant="outlined">
+            <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-password"
+              name='password'
+              type={showPassword ? 'text' : 'password'}
+              value={password}
+              onChange={handleChange}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={() => setShowPassword(!showPassword)}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                  >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              label="Password"
+            />
+          </FormControl>
+        </Box>
+        <Box className={styles.inputContainer}>
+        <FormControl sx={{ width: '400px'}} variant="outlined">
+            <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-conf-password"
+              name='passwordConf'
+              type={showConfPassword ? 'text' : 'password'}
+              value={passwordConf}
+              onChange={handleChange}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={() => setShowConfPassword(!showConfPassword)}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                  >
+                    {showConfPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              label="Password"
+            />
+          </FormControl>
+        </Box>
+        <Box className={styles.inputContainer}>
+          <TextField
+            fullWidth
+            label="Occupation"
+            type="text"
+            id="occupation"
+            name="occupation"
+            onChange={handleChange}
+          />
+        </Box>
+        <Box className={styles.inputContainer}>
+          <TextField
+            fullWidth
+            helperText="Date of Birth (MM/DD/YY)"
+            type="date"
+            id="dob"
+            name="dob"
+            onChange={handleChange}
+          />
+        </Box>
+        <Box className={styles.inputContainer}>
+          <TextField
+            fullWidth
+            label="Postal Zip Code"
+            type="number"
+            id="zip"
+            name="zip"
+            onChange={handleChange}
+          />
+        </Box>
+        <Box className={styles.inputContainer}>
+          <TextField
+            fullWidth
+            label="Education"
+            type="text"
+            id="education"
+            name="education"
+            onChange={handleChange}
+          />
+        </Box>
+        <Box className={styles.inputContainer}>
+          <Button disabled={isFormInvalid()} className={styles.button}>
+            Sign Up
+          </Button>
+          <Link to="/">
+            <Button color="warning">Cancel</Button>
+          </Link>
+        </Box>
+      </form>
+    </Paper>
   )
 }
 
