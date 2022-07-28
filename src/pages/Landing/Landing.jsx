b0be5359834ee313fa27ffcb79721094b942e4d8
whlong1 <whunterlong@gmail.com>
2022-07-27 22:25:34 -0400

    fix styling for about section on landing page

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -3,6 +3,7 @@ import logo from '../../assets/logo-icon.svg'
 import feynman from '../../assets/feynman.png'
 import rectangle from '../../assets/yellowRectangle.png'
 
+import Divider from '@mui/material/Divider'
 
 import Lottie from "lottie-react"
 import arrow from '../../assets/arrow-anim.json'
@@ -26,40 +27,42 @@ const Landing = ({ user }) => {
 
 
 
-      <div className='section-two'>
+      <div className='about-container'>
         <div className='about-header'>
           <img src={rectangle} alt="rectangle" style={{ height: 50, width: 50 }} />
           <h4>ABOUT US</h4>
           <h1>What the $%!# is Rubber Duck? </h1>
         </div>
-        <div className='flex'>
-          <div className='feynmanText' id="left-side">
+        <div className='about-content'>
+          <div className='about-textblock' style={{padding: '20px 20px 20px 80px'}}>
+            <h3>Why not some lorem ipsum?</h3>
             <p>
-              Rubber duck uses the Feynman Learning Technique for everyday learning tasks.
-              There are four key steps to the Feynman Technique:
-              Step 1 Choose a concept you want to learn about
-              Step 2 Explain it to a 12 year old
-              Step 3 Reflect, Refine, and Simplify
-              Step 4 Organize and Review
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut cursus elit. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur et convallis metus. Cras felis nisi, scelerisque dapibus orci semper, interdum egestas risus. Etiam molestie nibh ut erat pellentesque, non malesuada lorem sagittis.
             </p>
           </div>
-          <div className='whitespace'></div>
-          <div className='feynmanText' id="right-side">
+
+          <Divider style={{ background: 'black', width: '1px', opacity: '.25' }} />
+
+          <div className='about-textblock' style={{padding: '20px 80px 20px 20px'}}>
+            <h3>Why not some lorem ipsum?</h3>
             <p>
-              Richard Feynman was a Nobel prize-winning physicist. <br />His real superpower, however, was his ability to explain complicated subjects to others in simple terms. <br />He realized that jargon, vague words, and complexity reveal a lack of understanding.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut cursus elit. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur et convallis metus. Cras felis nisi, scelerisque dapibus orci semper, interdum egestas risus. Etiam molestie nibh ut erat pellentesque, non malesuada lorem sagittis.
             </p>
           </div>
         </div>
-        <div className='whitespace'></div>
       </div>
-      <div className='flex'>
+
+
+
+
+      {/* <div className='flex'>
         <img src={feynman} alt='feynman' style={{ width: 250, height: 250, borderRadius: 400 / 2, margin: 40 }} />
         <div className='feynmanText'>
           <h1><img src={rectangle} alt="rectangle" style={{ height: 50, width: 20 }} />The Feynman Technique</h1>
           <p>The Feynman Learning Technique is a simple way of approaching anything new you want to learn. Why use it? Because learning doesn't happen from skimming through a book or remembering enough to pass a test. Information is learned when you can explain it and use it in a wide variety of situations. The Feynman Technique gets more mileage from the ideas you encounter instead of rendering anything new into isolated, useless factoids.</p>
         </div>
       </div>
-      <div className='whitespace'></div>
+      <div className='whitespace'></div> */}
     </div>
   )
 }
