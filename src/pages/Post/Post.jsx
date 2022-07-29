ed48b7c8abcb6b11e3834dac1593faadf82098d4
whlong1 <whunterlong@gmail.com>
2022-07-28 22:45:31 -0400

    clean up Post.jsx

--- src/pages/Post/Post.jsx
+++ src/pages/Post/Post.jsx
@@ -1,8 +1,16 @@
 import { Link } from 'react-router-dom'
-import CardContent from '@mui/material/CardContent'
 import SvgIcon from '@mui/material/SvgIcon'
+import CardContent from '@mui/material/CardContent'
 import { ReactComponent as Diamond } from '../../assets/diamond-icon.svg'
-import { AuthTypography, BodyTypography, PostCard, DateTypography, StyledRating, StyledBoxSpaceBetween } from '../../styles/mui/StyledComponents'
+
+import {
+  PostCard,
+  StyledRating,
+  BodyTypography,
+  DateTypography,
+  AuthTypography,
+  StyledBoxSpaceBetween
+} from '../../styles/mui/StyledComponents'
 
 const Post = ({ user, post }) => {
   const { author } = post
@@ -17,21 +25,21 @@ const Post = ({ user, post }) => {
               {author?.name}
             </AuthTypography>
             <StyledRating
-              name="rating"
-              value={rating}
               readOnly
               max={4}
+              name="rating"
+              value={rating}
               defaultValue={4}
               icon={<SvgIcon fontSize='inherit'> <Diamond /> </SvgIcon>}
               emptyIcon={< SvgIcon fontSize='inherit'> <Diamond /> </SvgIcon>}
             />
           </StyledBoxSpaceBetween>
-          <BodyTypography> {text} </BodyTypography>
+          <BodyTypography>{text}</BodyTypography>
         </CardContent>
-        <DateTypography> 08/07/22 </DateTypography>
+        <DateTypography>08/07/22</DateTypography>
       </PostCard>
     </Link>
   )
 }
 
-export default Post;
+export default Post
