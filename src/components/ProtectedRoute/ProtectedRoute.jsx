ce53c8c2d29ca1b698cc56cfcd294bfac3974bc9
whlong1 <whunterlong@gmail.com>
2022-07-28 22:31:08 -0400

    build out protected route component

---
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -0,0 +1,7 @@
+import { Navigate } from 'react-router-dom'
+const ProtectedRoute = ({ user, children }) => {
+  if (!user) { return <Navigate to="/" /> }
+  return children
+}
+
+export default ProtectedRoute
