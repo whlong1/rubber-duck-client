3f20ccca5098cd146f799f258b354de33a2627bd
whlong1 <whunterlong@gmail.com>
2022-07-30 13:41:31 -0400

    update imports

--- src/pages/NewIteration/components/Analysis/Analysis.jsx
+++ src/pages/NewIteration/components/Analysis/Analysis.jsx
@@ -1,16 +1,15 @@
 import { useState, useEffect } from 'react'
-import Typography from '@mui/material/Typography'
+import AnalysisPopper from './AnalysisPopper'
+
 import Box from '@mui/material/Box'
 import Chip from '@mui/material/Chip'
-import writeGood from 'write-good'
-import Divider from '@mui/material/Divider'
 import Fade from '@mui/material/Fade'
 import Paper from '@mui/material/Paper'
-import AnalysisPopper from './AnalysisPopper'
+import Divider from '@mui/material/Divider'
+import Typography from '@mui/material/Typography'
 import { StyledAnalysisContainer, StyledFeedbackBox, StyledPaper } from '../../../../styles/mui/StyledComponents'
 
-// Utils
-import { writingTips } from './utils/utils'
+import { writeGood } from './utils/write-good/write-good'
 
 function Analysis({ text, keywords, handleClickSuggestion }) {
   const [tips, setTips] = useState([])
@@ -19,7 +18,7 @@ function Analysis({ text, keywords, handleClickSuggestion }) {
   useEffect(() => {
     const timer = setTimeout(() => {
       setWarnings(writeGood(text))
-      setTips(writeGood(text, { checks: writingTips }))
+      // setTips(writeGood(text, { checks: writingTips }))
     }, 1000)
     return () => clearTimeout(timer)
   }, [text])
@@ -35,72 +34,72 @@ function Analysis({ text, keywords, handleClickSuggestion }) {
       <StyledAnalysisContainer>
         <StyledFeedbackBox>
           <Paper elevation={2} sx={{ minWidth: '100%', minHeight: '95%', padding: '.5rem' }}>
-        {tips.map((obj, idx) => (
-          <Box sx={{ display: 'flex', alignItems: "center", marginBottom: '5px'}} key={idx}>
-            <Fade in={true} timeout={1000}>
-              <Chip 
-                label="suggestion" 
-                variant="outlined" 
-                color="warning" 
-                style={{height: '20px', marginRight: '10px'}} 
-              />
-            </Fade>
-            <Fade in={true} timeout={1000}>
-              <Typography style={{ color: "#ffffff" }}>
-                {shapeText(obj.reason)[0]}&nbsp;
-              </Typography>
-            </Fade>
-            <Fade in={true} timeout={1000}>
-              <Typography style={{ color: "#bcbcbc" }}>
-                {shapeText(obj.reason)[1]}
-              </Typography>
-            </Fade>
-          </Box>
-        ))}
-        {warnings.map((obj, idx) => (
-          <Box 
-            sx={{
-              display: 'flex', 
-              alignItems: "center", 
-              marginBottom: '5px'
-            }} 
-            key={idx}
-          >
-            <Fade in={true} timeout={1000}>
-              <Chip 
-                label="warning" 
-                variant="outlined" 
-                color="error" 
-                style={{ height: '20px', width:'80px', marginRight: '10px'}}
-              />
-            </Fade>
-            <Fade in={true} timeout={1000}>
-              <Typography style={{ color: "#ffffff" }}>
-                {shapeText(obj.reason)[0]}&nbsp;
-              </Typography>
-            </Fade>
-            <Fade in={true} timeout={1000}>
-              <Typography style={{ color: "#bcbcbc"}}>
-                {shapeText(obj.reason)[1]}
-              </Typography>
-            </Fade>
-          </Box>
-        ))}
-        </Paper>
+            {tips.map((obj, idx) => (
+              <Box sx={{ display: 'flex', alignItems: "center", marginBottom: '5px' }} key={idx}>
+                <Fade in={true} timeout={1000}>
+                  <Chip
+                    label="suggestion"
+                    variant="outlined"
+                    color="warning"
+                    style={{ height: '20px', marginRight: '10px' }}
+                  />
+                </Fade>
+                <Fade in={true} timeout={1000}>
+                  <Typography style={{ color: "#ffffff" }}>
+                    {shapeText(obj.reason)[0]}&nbsp;
+                  </Typography>
+                </Fade>
+                <Fade in={true} timeout={1000}>
+                  <Typography style={{ color: "#bcbcbc" }}>
+                    {shapeText(obj.reason)[1]}
+                  </Typography>
+                </Fade>
+              </Box>
+            ))}
+            {warnings.map((obj, idx) => (
+              <Box
+                sx={{
+                  display: 'flex',
+                  alignItems: "center",
+                  marginBottom: '5px'
+                }}
+                key={idx}
+              >
+                <Fade in={true} timeout={1000}>
+                  <Chip
+                    label="warning"
+                    variant="outlined"
+                    color="error"
+                    style={{ height: '20px', width: '80px', marginRight: '10px' }}
+                  />
+                </Fade>
+                <Fade in={true} timeout={1000}>
+                  <Typography style={{ color: "#ffffff" }}>
+                    {shapeText(obj.reason)[0]}&nbsp;
+                  </Typography>
+                </Fade>
+                <Fade in={true} timeout={1000}>
+                  <Typography style={{ color: "#bcbcbc" }}>
+                    {shapeText(obj.reason)[1]}
+                  </Typography>
+                </Fade>
+              </Box>
+            ))}
+          </Paper>
         </StyledFeedbackBox>
         <Divider sx={{ fontSize: '1rem', marginTop: '.5rem' }} flexItem />
         <Box sx={{ marginTop: '.5rem', width: '100%', padding: '.5rem' }}>
-        { !!keywords.length && <Typography>Suggestions:</Typography> }
+          {!!keywords.length && <Typography>Suggestions:</Typography>}
         </Box>
         <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: '10px', paddingTop: '10px' }}>
           {keywords.map((word, idx) => (
-              <Chip 
-                key={idx} 
-                label={word}
-                variant='outlined'
-                color='success'
-                onClick={() => handleClickSuggestion(word)}
-              />
+            <Chip
+              key={idx}
+              label={word}
+              variant='outlined'
+              color='success'
+              onClick={() => handleClickSuggestion(word)}
+            />
           ))}
         </Box>
       </StyledAnalysisContainer>
