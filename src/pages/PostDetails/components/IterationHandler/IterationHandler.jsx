89111295059e51949d3a8487e196a8109cbde7b8
whlong1 <whunterlong@gmail.com>
2022-08-08 11:09:32 -0400

    add commentsection

--- src/pages/PostDetails/components/IterationHandler/IterationHandler.jsx
+++ src/pages/PostDetails/components/IterationHandler/IterationHandler.jsx
@@ -3,16 +3,20 @@ import { useState } from "react"
 import SideNav from "./SideNav"
 import VoteHandler from "./VoteHandler"
 import IterationDisplay from "./IterationDisplay"
+import CommentSection from '../CommentSection/CommentSection'
 
 const IterationHandler = ({ user, postId, iterations }) => {
   const [index, setIndex] = useState(0)
   const [iterationsArr, setIterationsArr] = useState(iterations)
   const selectedIteration = iterationsArr[index]
 
+  console.log(selectedIteration)
+
   return (
     <div style={{ display: 'flex' }}>
       <SideNav index={index} setIndex={setIndex} iterations={iterationsArr} />
       <IterationDisplay iteration={selectedIteration} />
+      <CommentSection />
       <VoteHandler
         user={user}
         postId={postId}
